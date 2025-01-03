08bad5e206cf2c73866f75b37dd63972545bae60
SNEHA C SHAJI <snehacshaji202@gmail.com>
2025-01-04 02:33:41 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import profilePic from "../assets/images/perfil.png"
 
 const Home = () => {
@@ -7,69 +8,68 @@ const Home = () => {
       id="home"
       className="h-screen flex items-center justify-center  "
     >
-      <div className="container mx-auto px-6 flex flex-col md:flex-row items-center">
+      <div className="container mx-auto px-6 flex flex-col md:flex-row items-center" style={{ fontFamily: "Poppins, sans-serif" }}>
         {/* Left Side */}
-        <div className="text-center md:text-left md:w-1/2 space-y-4">
-          <h1 className="text-4xl md:text-6xl font-bold text-gray-800">
+        <div className="text-center md:text-left md:w-1/2 space-y-4" >
+          <h1 className="text-4xl md:text-6xl font-bold text-slate-800">
             Navigating
           </h1>
           <p>
-          <h1 className="text-4xl md:text-6xl font-bold text-blue-600">
+          <h1 className="text-4xl md:text-6xl font-bold text-slate-500">
            Collaborations
           </h1>
           </p>
           <p>
-          <h5 className=" font-bold">
-          Exploring business development strategies
-          in the IT industry.
-
+          <h5 className=" font-bold text-lg">
+          Exploring business development strategies in SaaS.
           </h5>
           </p>
-          <p className="text-lg text-gray-600">
-          With a solid foundation in B2B sales, engagement automation,
-and account management, I bring a goal-oriented approach to
-exploring innovative technological solutions for marketing
-and business challenges. Proficient in leveraging tools
-like LinkedIn Sales Navigator, HubSpot, Salesforce, and AI,
-I excel in driving lead generation, optimizing campaigns,
-and delivering impactful results.
 
-
+          <p className="text-lg text-slate-600">
+          With a solid foundation in B2B sales, engagement automation, and account management, 
+          a goal-oriented approach is applied to exploring innovative technological solutions for marketing and business challenges.
+           Leveraging tools such as LinkedIn Sales Navigator, HubSpot, Salesforce, and AI enables driving lead generation, optimizing campaigns, 
+           and delivering impactful results.
           </p>
+
           <div>
-            <button className="bg-blue-600 text-white px-6 py-3 rounded-lg shadow hover:bg-blue-700">
+          <a href="#contact">
+            <button className="bg-slate-600 text-white px-6 py-3 rounded-lg shadow hover:bg-slate-700">
               Contact Me
             </button>
+          </a>
           </div>
+
           {/* Social Links */}
           <div className="flex space-x-4 justify-center md:justify-start mt-4">
             <a
-              href="#"
-              className="text-gray-500 hover:text-blue-600"
+              href="https://www.linkedin.com/in/sneha-c-shaji/"
+              className="text-gray-500 hover:text-slate-800"
               aria-label="LinkedIn"
             >
               <i className="fab fa-linkedin fa-2x"></i>
             </a>
             <a
-              href="#"
-              className="text-gray-500 hover:text-blue-600"
+              href="https://github.com/sneha2180"
+              className="text-gray-500 hover:text-slate-800"
               aria-label="GitHub"
             >
               <i className="fab fa-github fa-2x"></i>
             </a>
-            <a
+            {/* <a
               href="#"
-              className="text-gray-500 hover:text-blue-600"
+              className="text-gray-500 hover:text-slate-800"
               aria-label="Twitter"
             >
               <i className="fab fa-twitter fa-2x"></i>
-            </a>
+            </a> */}
           </div>
         </div>
 
         {/* Right Side */}
       <div className="mt-8 md:mt-0 md:w-1/2 flex justify-center">
-  <div className="w-100 h-100 bg-blue-600 relative overflow-hidden" style={{
+  <div className="w-100 h-100 bg-slate-800 relative overflow-hidden" style={{
+      fontFamily: "Poppins, sans-serif",
       borderTopLeftRadius: '50%',
       borderTopRightRadius: '30%',
       borderBottomLeftRadius: '40%',
